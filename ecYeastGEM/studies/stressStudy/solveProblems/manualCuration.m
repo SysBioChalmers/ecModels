9cdc2d358fe2dc0e63b84510d58e5ef3cc370a74
benjasanchez <bensan@chalmers.se>
2019-01-02 17:13:15 +0100

    fix: remove manual curation based on iterative fitting

--- ecYeastGEM/studies/stressStudy/solveProblems/manualCuration.m
+++ ecYeastGEM/studies/stressStudy/solveProblems/manualCuration.m
@@ -2,8 +2,7 @@
 
 function model = manualCuration(model)
 
-to_change = {'r_0118No1','r_1010No1','r_1011No1','r_0904No1','r_0476No1', ...
-             'r_1042No1','r_1043No1','r_0225No1','r_2182No1','r_2183No1'};
+to_change = {'r_0118No1','r_1010No1','r_1011No1','r_0904No1','r_2182No1','r_2183No1'};
 
 for i = 1:length(model.rxns)
     if sum(strcmp(to_change,model.rxns{i})) > 0
@@ -35,14 +34,8 @@ for i = 1:length(model.rxns)
             if strcmp(model.rxns{i},'r_1010No1') || strcmp(model.rxns{i},'r_1011No1')
                 model.S(int_pos(j),i) = -(0.322*3600)^-1;    %0.322 [1/s]
             end
-            
-            % 3. Glutamine synthetase (P32288/EC6.3.1.2): Value is too low for condition
-            % EtOH-60. Did iterative fitting = 1.4 (2017-04-26)
-            if strcmp(model.rxns{i},'r_0476No1')
-                model.S(int_pos(j),i) = -(236*1.4*60*1e3/1e3*MW_set)^-1;    %236 [umol/min/mg]
-            end
 
-            % 4. Phosphomevalonate kinase (P24521/EC2.7.4.2):
+            % 3. Phosphomevalonate kinase (P24521/EC2.7.4.2):
             % The match by GECKO was done to ATP instead of phosphomevalonate because
             % the name in the model was different ((R)-5-phosphomevalonic acid). The
             % value was changed manually to only kcat available for phosphomevalonate
@@ -52,20 +45,7 @@ for i = 1:length(model.rxns)
                 model.S(int_pos(j),i) = -(10.2*3600)^-1;    %10.2 [1/s]
             end
             
-            % 5. Threonine--tRNA ligase, cytoplasmic & mitochondrial
-            % (P04801-P07236/EC6.1.1.3): Value is too low for condition EtOH-40. Did
-            % iterative fitting = 1.1 (2017-04-26).
-            if strcmp(model.rxns{i},'r_1042No1') || strcmp(model.rxns{i},'r_1043No1')
-                model.S(int_pos(j),i) = -(3.32*1.1*3600)^-1;    %3.32 [1/s]
-            end
-            
-            % 6. ATP phosphoribosyltransferase (P00498/EC2.4.2.17): Value is too low
-            % for conditions Osm0.8/1.0/1.2. Did iterative fitting = 1.2 (2017-04-27).
-            if strcmp(model.rxns{i},'r_0225No1')
-                model.S(int_pos(j),i) = -(2.7*1.2*3600)^-1;    %2.7 [1/s]
-            end
-            
-            % 7. Acyl-CoA desaturase 1 (P21147/EC1.14.19.1): Low kcat values and non of
+            % 4. Acyl-CoA desaturase 1 (P21147/EC1.14.19.1): Low kcat values and non of
             % them for yeast. Value corrected with s.a. from Arabidopsis sp. (2017-04-27).
             % Ref: https://www.ncbi.nlm.nih.gov/pmc/articles/PMC55441/
             if strcmp(model.rxns{i},'r_2182No1') || strcmp(model.rxns{i},'r_2183No1')
