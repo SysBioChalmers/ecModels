d8275ad7ce0039487f85ba469c3f8d66e4982caf
Ivan Domenzain <ivand@chalmers.se>
2019-02-05 19:27:28 +0100

    feat: run FVA included in GECKO for analysis

--- ecYeastGEM/studies/yeast8/FVA_comparison/getYeast8FVAfigure.m
+++ ecYeastGEM/studies/yeast8/FVA_comparison/getYeast8FVAfigure.m
@@ -1,10 +1,42 @@
-function [distributions, stats] = getYeast8FVAfigure(model,ecModel)
-c_source = 'D-glucose exchange';
-[FVA_chemostat,~,stats_c] = comparativeFVA(model,ecModel,c_source,true,0,'oxygen');
-[FVA_batch,~,stats_b] = comparativeFVA(model,ecModel,c_source,false,0,'oxygen');
+% getYeast8FVAfigure
+%   
+%   Ivan Domenzain, 2018-12-11
+%
+current = pwd;
+c_source = 'r_1714';
+%Clone the GECKO repository
+git('clone https://github.com/SysBioChalmers/GECKO.git')
+%Clone the yeast-GEM repository
+git('clone https://github.com/SysBioChalmers/yeast-GEM.git')
+%Load yeastGEM and constrain it with Y6 media
+cd yeast-GEM/ModelFiles/mat
+load ('yeastGEM.mat')
+cd ../../ComplementaryScripts/modelCuration
+model = minimal_Y6(model);
+%Load ecYeastGEM
+cd (current)
+load ('../../../model/ecYeastGEM_batch.mat')
+cd GECKO/geckomat/utilities/FVA
+%Run comparative FVA for chemostat conditions
+[FVA_chemostat,~,stats_c] = comparativeFVA(model,ecModel_batch,c_source,true,0);
+%Run comparative FVA for batch conditions
+[FVA_batch,~,stats_b]     = comparativeFVA(model,ecModel_batch,c_source,false,0);
+%convert zeros to low values to get a visually nice plot
+FVA_chemostat = filterDistributions(FVA_chemostat,1E-10);
+FVA_batch     = filterDistributions(FVA_batch,1E-10);
 distributions = {FVA_chemostat{1}, FVA_chemostat{2},FVA_batch{1},FVA_batch{2}};
 stats         = {stats_c,stats_b}; 
 legends       = {'model-chemostat', 'ecModel-chemostat','model-batch', 'ecModel-batch'};
 titleStr      = 'Flux variability cumulative distribution';
 [~, ~]        = plotCumDist(distributions,legends,titleStr);
+%Remove the cloned repos:
+rmdir('GECKO', 's')
+rmdir('yeast-GEM', 's')
+
+function filterDistributions(distribution,treshold)
+for i=1:length(distribution)
+    dist = distribution{i};
+    dist(dist<treshold) =treshold;
+    dist(find(dist==1E-10,1)) = treshold/10; 
+end
 end
