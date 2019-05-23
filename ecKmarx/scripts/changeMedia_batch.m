aecbb7dd21658e5e745c03361af93f3626e3eeb5
Ivan Domenzain <ivand@chalmers.se>
2019-05-23 17:43:18 +0200

    fix: modify changeMedia_batch

--- ecKmarx/scripts/changeMedia_batch.m
+++ ecKmarx/scripts/changeMedia_batch.m
@@ -18,6 +18,9 @@ function [model,pos] = changeMedia_batch(model,c_source,media,flux)
 % Give the carbon source (c_source) input variable with the following
 % format: c_source  = 'D-glucose exchange (reversible)'
 
+if nargin<3
+    media = 'Min';
+end
 %first block any uptake
 [rxnIDs,exchange]  = getExchangeRxns(model);
 %Exclude protein pool from exchange reactions list
@@ -64,14 +67,13 @@ for i = 1:N
     model.ub(pos(i)) = flux(i);
 end
 gIndex = find(model.c);
-gIndex = find(model.c);
 model.ub(gIndex) = Inf;
 %Allow uptake of essential components
 model = setParam(model, 'ub', 'r_1727_REV', Inf); % 'ammonium exchange';
 model = setParam(model, 'ub', 'r_1723_REV', Inf); % 'water exchange' ;
 model = setParam(model, 'ub', 'r_1731_REV', Inf); % 'iron(2+) exchange';
 model = setParam(model, 'ub', 'r_1725_REV', Inf); % 'oxygen exchange';
-model = setParam(model, 'ub', 'r_1729_REV', Inf); % 'phosphate exchange';
+model = setParam(model, 'ub', 'r_1729_REV', 0.1); % 'phosphate exchange';
 model = setParam(model, 'ub', 'r_1728_REV', Inf); % 'sulphate exchange';
 model = setParam(model, 'ub', 'r_1724_REV', Inf); % 'H+ exchange' ;
 model = setParam(model, 'ub', 'r_1772_REV', Inf); % 'Biotin exchange' ;
@@ -86,29 +88,4 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function pos = getComponentIndexes(model,c_source)
     pos(1)  = find(strcmpi(model.rxnNames,c_source));
-    pos(2)  = find(strcmpi(model.rxnNames,'L-alanine exchange (reversible)'));
-    pos(3)  = find(strcmpi(model.rxnNames,'L-arginine exchange (reversible)'));
-    pos(4)  = find(strcmpi(model.rxnNames,'L-asparagine exchange (reversible)'));
-    pos(5)  = find(strcmpi(model.rxnNames,'L-aspartate exchange (reversible)'));
-    pos(6)  = find(strcmpi(model.rxnNames,'L-cysteine exchange (reversible)'));
-    pos(7)  = find(strcmpi(model.rxnNames,'L-glutamine exchange (reversible)'));
-    pos(8)  = find(strcmpi(model.rxnNames,'L-glutamate exchange (reversible)'));
-    pos(9)  = find(strcmpi(model.rxnNames,'L-glycine exchange (reversible)'));
-    pos(10) = find(strcmpi(model.rxnNames,'L-histidine exchange (reversible)'));
-    pos(11) = find(strcmpi(model.rxnNames,'L-isoleucine exchange (reversible)'));
-    pos(12) = find(strcmpi(model.rxnNames,'L-leucine exchange (reversible)'));
-    pos(13) = find(strcmpi(model.rxnNames,'L-lysine exchange (reversible)'));
-    pos(14) = find(strcmpi(model.rxnNames,'L-methionine exchange (reversible)'));
-    pos(15) = find(strcmpi(model.rxnNames,'L-phenylalanine exchange (reversible)'));
-    pos(16) = find(strcmpi(model.rxnNames,'L-proline exchange (reversible)'));
-    pos(17) = find(strcmpi(model.rxnNames,'L-serine exchange (reversible)'));
-    pos(18) = find(strcmpi(model.rxnNames,'L-threonine exchange (reversible)'));
-    pos(19) = find(strcmpi(model.rxnNames,'L-tryptophan exchange (reversible)'));
-    pos(20) = find(strcmpi(model.rxnNames,'L-tyrosine exchange (reversible)'));
-    pos(21) = find(strcmpi(model.rxnNames,'L-valine exchange (reversible)'));
-    pos(22) = find(strcmpi(model.rxnNames,'2''-deoxyadenosine exchange (reversible)'));
-    pos(23) = find(strcmpi(model.rxnNames,'2''-deoxyguanosine exchange (reversible)'));
-    pos(24) = find(strcmpi(model.rxnNames,'thymidine exchange (reversible)'));
-    pos(25) = find(strcmpi(model.rxnNames,'deoxycytidine exchange (reversible)'));
-    pos(26) = find(strcmpi(model.rxnNames,'D-glucose exchange (reversible)'));
 end
