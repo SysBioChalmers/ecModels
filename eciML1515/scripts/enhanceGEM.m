96e0136ca213f17156b827257df0a528230e2f3d
Ivan Domenzain <ivand@chalmers.se>
2019-06-02 17:01:48 +0200

    style: remove unnecessary lines

--- eciML1515/scripts/enhanceGEM.m
+++ eciML1515/scripts/enhanceGEM.m
@@ -1,38 +1,29 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % [ecModel,ecModel_batch] = enhanceGEM(model,toolbox,name,version)
 %
-% Ivan Domenzain. Last edited: 2019-05-28
+% Ivan Domenzain. Last edited: 2019-06-02
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function [ecModel,ecModel_batch,version] = enhanceGEM(model,toolbox,name)
-
-if nargin < 3
-    name    = '';
-end
+function [ecModel,ecModel_batch,version] = enhanceGEM(model,toolbox)
 
 %Provide your organism scientific name
 org_name = 'escherichia coli';
-
 %Convert model to RAVEN for easier visualization later on:
 format short e
 if isfield(model,'rules')
     initCobraToolbox
     model = ravenCobraWrapper(model);
 end
-
 %Remove blocked rxns + correct model.rev:
 cd change_model
-[model,name,version] = preprocessModel(model,name);
-
+[model,name,version] = preprocessModel(model);
 %Retrieve kcats & MWs for each rxn in model:
 cd ../get_enzyme_data
 model_data = getEnzymeCodes(model);
 kcats      = matchKcats(model_data,org_name);
-
 %Integrate enzymes in the model:
 cd ../change_model
 ecModel                 = readKcatData(model_data,kcats);
 [ecModel,modifications] = manualModifications(ecModel);
-
 %Constrain model to batch conditions:
 sigma    = 0.5;      %Optimized for glucose
 Ptot     = 0.5793;      %Assumed constant
@@ -41,13 +32,10 @@ c_source = 'D-glucose exchange (reversible)'; %Rxn name for the glucose uptake r
 cd ../limit_proteins
 [ecModel_batch,OptSigma] = getConstrainedModel(ecModel,c_source,sigma,Ptot,gR_exp,modifications,name);
 disp(['Sigma factor (fitted for growth on glucose): ' num2str(OptSigma)])
-
 %Save output models:
 cd ../../models
 ecModel = saveECmodel(ecModel,toolbox,name,version);
 ecModel_batch = saveECmodel(ecModel_batch,toolbox,[name '_batch'],version);
 cd ../geckomat
-
 end
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
