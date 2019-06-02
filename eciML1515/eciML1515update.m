2b01584fa6d99bf0b70c619d88dc89e401cc832f
Ivan Domenzain <ivand@chalmers.se>
2019-06-02 16:57:29 +0200

    fix: avoid removal of original model .xml file

--- eciML1515/eciML1515update.m
+++ eciML1515/eciML1515update.m
@@ -1,6 +1,6 @@
 % eciML1515Update
 %
-%  Ivan Domenzain.  2019-05-28
+%  Ivan Domenzain.  2019-06-02
 %
 
 %Clone the necessary repos:
@@ -10,35 +10,37 @@ cd model
 model    = importModel('iML1515.xml');
 cd ..
 %Replace scripts in GECKO:
-fileNames = dir('scripts');
-for i = 1:length(fileNames)
-    fileName = fileNames(i).name;
-    if ~strcmp(fileName,'.') && ~strcmp(fileName,'..') && ~strcmp(fileName,'.DS_Store')
-        fullName   = ['scripts/' fileName];
-        GECKO_path = dir(['GECKO/**/' fileName]);
-        GECKO_path = GECKO_path.folder;
-        copyfile(fullName,GECKO_path)
-    end
-end
-
+ replaceFiles('scripts','GECKO/**/');
+%Replace databases in GECKO:
+ replaceFiles('databases','GECKO/databases/');
+%Remove unecessary files
+ delete('GECKO/databases/prot_abundance.txt')
 %Run GECKO pipeline:
-cd GECKO
+cd GECKO/geckomat
 GECKOver = git('describe --tags');
-cd geckomat
 [ecModel,ecModel_batch,version] = enhanceGEM(model,'COBRA');
 cd ../..
-
 %Move model files:
-rmdir('model', 's')
 movefile GECKO/models/eciML1515 model
 save('model/eciML1515.mat','ecModel')
 save('model/eciML1515_batch.mat','ecModel_batch')
-
 %Save associated versions:
 fid = fopen('dependencies.txt','wt');
 fprintf(fid,['GECKO\t' GECKOver '\n']);
 fprintf(fid,['iML1515\t' version '\n']);
 fclose(fid);
-
 %Remove the cloned repos:
 rmdir('GECKO', 's')
+% % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function replaceFiles(fileType,path)
+fileNames = dir(fileType);
+for i = 1:length(fileNames)
+    fileName = fileNames(i).name;
+    if ~strcmp(fileName,'.') && ~strcmp(fileName,'..') && ~strcmp(fileName,'.DS_Store')
+        fullName   = [fileType '/' fileName];
+        GECKO_path = dir([path fileName]);
+        GECKO_path = GECKO_path.folder;
+        copyfile(fullName,GECKO_path)
+    end
+end
+end
