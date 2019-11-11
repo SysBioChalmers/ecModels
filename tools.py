d39392c84e71a8d02ed1054aa37bb9cffc65a301
Mihail Anton <mihail.anton@chalmers.se>
2019-11-11 16:38:10 +0100

    fix: deal with matlab version

--- tools.py
+++ tools.py
@@ -100,7 +100,7 @@ class GECKO_VM:
     def check_dependencies(self):
         # Check Matlab version
         cmd = sp.check_output(['/usr/local/bin/matlab', '-nodisplay -nosplash -nodesktop -r', '"disp(version); quit"'])
-        m_version = ' '.join(cmd.decode('utf-8').split()[-3:-1])
+        m_version = ' '.join(cmd.decode('utf-8').split()[-2:-1])
         if m_version != self.version('MATLAB'):
             self.HAS_CHANGES = True
             l.warning('MATLAB changed from {} to {}'.format(self.version('MATLAB'), m_version))
