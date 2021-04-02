dab7ca528d140a4726c988995e97c3b46d5fc3b8
Ivan Domenzain <ivand@chalmers.se>
2021-04-02 16:54:51 +0200

    fix: enable generation of ecModel_batch and saving steps

--- ecMouseGEM/scripts/enhanceGEM.m
+++ ecMouseGEM/scripts/enhanceGEM.m
@@ -68,15 +68,21 @@ ecModel = readKcatData(model_data,kcats);
 if ~isdir(['../../models/' name])
     mkdir(['../../models/' name])
 end
+%Constrain model to batch conditions:
+fprintf('\n==============================================================')
+fprintf('\nGenerating ecModel with shared pool assumption (ecModel_batch):')
+fprintf('\n==============================================================\n')
+cd ../limit_proteins
+[ecModel_batch,OptSigma] = getConstrainedModel(ecModel,modifications,name);
+disp(['Sigma factor (fitted for growth on glucose): ' num2str(OptSigma)])
 
 %Save output models:
-%fprintf('\n=============')
-%fprintf('\nSaving models:')
-%fprintf('\n=============\n')
-%cd ../../models
-%ecModel = saveECmodel(ecModel,toolbox,name,modelVer);
-ecModel_batch = ecModel;
-%ecModel_batch = saveECmodel(ecModel_batch,toolbox,[name '_batch'],modelVer);
-cd ..
+fprintf('\n=============')
+fprintf('\nSaving models:')
+fprintf('\n=============\n')
+cd ../../models
+ecModel = saveECmodel(ecModel,toolbox,name,modelVer);
+ecModel_batch = saveECmodel(ecModel_batch,toolbox,[name '_batch'],modelVer);
+cd ../geckomat
 
 end
